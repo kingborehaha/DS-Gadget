380ac43070fcb933a408352934846fbba363b281
kingborehaha <55667610+kingborehaha@users.noreply.github.com>
2021-05-19 00:12:06 -0700

    Add files via upload

---
+++ DS Gadget/GetTxtResourceClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DS_Gadget
+{
+    class GetTxtResourceClass
+    {
+
+        //new
+        public static string GetTxtResource(string filePath)
+        {
+
+            Path.Combine(Environment.CurrentDirectory, filePath);
+
+            string fileString = File.ReadAllText(filePath);
+
+            //fileString.Replace("\n\n", ""); //remove any empty new lines
+            //need a better way of just removing superfluous lines to prevent a dumbly easy error to run into
+
+            return fileString;
+
+        }
+
+    }
+}
