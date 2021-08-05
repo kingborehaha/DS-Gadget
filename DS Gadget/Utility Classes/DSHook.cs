783fd6d62774a9081cd4ff6e686ef7f78dd28d23
Norgaren <83100346+Nordgaren@users.noreply.github.com>
2021-08-05 16:19:59 -0700

    Updated Hook

--- DS Gadget/Utility Classes/DSHook.cs
+++ DS Gadget/Utility Classes/DSHook.cs
@@ -482,7 +482,11 @@ namespace DS_Gadget
                     var Attr = prop.GetCustomAttribute<ControlAttribute>();
                     if (Attr != null && Attr.Name == attributeName)
                     {
-                        prop.SetValue(this, (byte)value, null); //Set the properties value
+                        if (prop.GetValue(this) is byte)
+                            prop.SetValue(this, (byte)value, null); //Set the properties value
+                        else
+                            prop.SetValue(this, value, null); //Set the properties value
+
                     }
                 }
             }
