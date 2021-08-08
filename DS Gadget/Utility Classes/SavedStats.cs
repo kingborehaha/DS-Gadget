9cfc493736e8e475495ad12ef7e795829f0711c4
Norgaren <83100346+Nordgaren@users.noreply.github.com>
2021-08-08 11:28:39 -0700

    Fixed SavesStats loading default values

--- DS Gadget/Utility Classes/SavedStats.cs
+++ DS Gadget/Utility Classes/SavedStats.cs
@@ -5,6 +5,7 @@ namespace DS_Gadget
 {
     class SavedStats
     {
+        [Control("cmbSex")]
         public string Name { get; set; }
         [Control("cmbSex")]
         public int? Sex { get; set; }
